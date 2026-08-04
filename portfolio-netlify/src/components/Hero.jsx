becded85de2b8c717e1c7c716133d79be6d81133
rohithreddych2 <rchukka1@aum.edu>
2026-08-04 17:35:28 -0500

    Update Hero.jsx

--- portfolio-netlify/src/components/Hero.jsx
+++ portfolio-netlify/src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero({ profile, education }) {
         </h1>
         <p className="lead">{profile.tagline}</p>
         <p className="meta">
-          <span>📍 {profile.location}</span>
+          <span>phone : +1(205) 377 8565 </span>
           <span>✉️ <a href={`mailto:${profile.email}`}>{profile.email}</a></span>
           {education && (
             <span>🎓 {education.degree}, {education.school}</span>
